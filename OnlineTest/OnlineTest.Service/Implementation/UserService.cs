dd4f2e6293fa598dfe40f9d5eb413aa7581a9155
jack <ccm1st@gmail.com>
2019-04-07 18:45:26 +1000

    - Fixed Namespace

--- OnlineTest/OnlineTest.Service/Implementation/UserService.cs
+++ OnlineTest/OnlineTest.Service/Implementation/UserService.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace OnlineTest.Service.Implementation.Service
+namespace OnlineTest.Service.Implementation
 {
     public class UserService : IUserService
     {
