a65a625cbd3bfb6f03aaaa0cc1f04639495a22de
jack <ccm1st@gmail.com>
2019-04-07 17:53:22 +1000

    - Add models for excercise 2

---
+++ OnlineTest/OnlineTest.Service/Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineTest.Service.Domain
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ OnlineTest/OnlineTest.Service/Domain/ShopperHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineTest.Service.Domain
+{
+    public class ShopperHistory
+    {
+        public int CustomerId { get; set; }
+        public List<Product> PurchasedProducts { get; set; }
+    }
+}
